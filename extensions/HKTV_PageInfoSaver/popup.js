a75ca1feae57006edb427e65437890bbccf34559
taylor <judy1998taylor@qq.com>
2024-08-14 15:06:27 +0800

    add 显示图片和下载按钮

--- extensions/HKTV_PageInfoSaver/popup.js
+++ extensions/HKTV_PageInfoSaver/popup.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   var saveButton = document.getElementById("save-button");
-  var downloadButton = document.getElementById("download-button");
-
   var saveDisplay = document.getElementById("save-display");
-  var downloadDisplay = document.getElementById("download-display");
   var isProcessing = false;
+  var productCode;
 
-  var productCode
-
-  saveButton.addEventListener("click", function () {
-    if (isProcessing) {
-      saveDisplay.textContent = "save操作正在进行中，请稍候...";
-      console.log("save操作正在进行中，请稍候...");
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (chrome.runtime.lastError) {
+      console.error("查询标签页时出错：", chrome.runtime.lastError.message);
+      saveDisplay.textContent = "获取页面信息时出错，请重试。";
+      resetProcessingState();
       return;
     }
+    const url = tabs[0].url;
+    const lang = isEnglishPage(url) ? "en" : "zh";
 
-    isProcessing = true;
-    saveButton.disabled = true; // 禁用按钮
-
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (chrome.runtime.lastError) {
-        console.error("查询标签页时出错：", chrome.runtime.lastError.message);
-        saveDisplay.textContent = "获取页面信息时出错，请重试。";
-        resetProcessingState();
+    saveButton.addEventListener("click", function () {
+      if (isProcessing) {
+        saveDisplay.textContent = "save操作正在进行中，请稍候...";
+        console.log("save操作正在进行中，请稍候...");
         return;
       }
+
+      isProcessing = true;
+      saveButton.disabled = true; // 禁用按钮
 
       chrome.scripting.executeScript(
         {
@@ -44,23 +42,24 @@ document.addEventListener("DOMContentLoaded", function () {
             let obj_extractInfoFromUrl = extractInfoFromUrl(tabs[0].url);
 
             let infoObject = {
-              pageTitle: tabs[0].title,
               productName: obj_injectionResults.productName,
               productBrand: obj_injectionResults.productBrand,
               shortDesc: obj_injectionResults.shortDesc,
               origin: obj_injectionResults.origin,
               categoryPath: obj_extractInfoFromUrl.categoryPath,
-              productShopId: obj_extractInfoFromUrl.productShopId,
-              productSKU: obj_extractInfoFromUrl.productSKU,
-              url: decodeURIComponent(tabs[0].url),
-              imgsUrls: obj_injectionResults.imgsUrls,
+              // url: decodeURIComponent(tabs[0].url),
+              // imgsUrls: obj_injectionResults.imgsUrls,
             };
 
             let content = generateContent(infoObject);
             saveDisplay.textContent = content;
 
+            // 显示图片和下载按钮
+            // displayImagesWithDownloadButtons(obj_injectionResults.imgsUrls.split('\n'));
+            displayImagesWithDownloadButtons(obj_injectionResults.imgsUrls);
+
             // 保存信息到本地文件
-            savePageInfoToFile(content);
+            savePageInfoToFile(content, lang);
           } else {
             console.error("未能获取页面信息");
             saveDisplay.textContent = "未能获取页面信息，请重试。";
@@ -72,53 +71,52 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  downloadButton.addEventListener("click", function () {
-    if (isProcessing) {
-      downloadDisplay.textContent = "download操作正在进行中，请稍候...";
-      console.log("download操作正在进行中，请稍候...");
-      return;
-    }
+  function displayImagesWithDownloadButtons(imageUrls) {
+    const container = document.getElementById("image-container");
+    container.innerHTML = ""; // 清空容器
 
-    isProcessing = true;
-    downloadButton.disabled = true; // 禁用按钮
+    imageUrls.forEach((url, index) => {
+      const wrapper = document.createElement("div");
+      wrapper.style.marginBottom = "10px";
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (chrome.runtime.lastError) {
-        console.error("查询标签页时出错：", chrome.runtime.lastError.message);
-        saveDisplay.textContent = "获取页面信息时出错，请重试。";
-        resetProcessingState();
-        return;
-      }
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          func: getProductImagesUrls,
-        },
-        (injectionResults) => {
-          if (chrome.runtime.lastError) {
-            console.error("执行脚本时出错：", chrome.runtime.lastError.message);
-            saveDisplay.textContent = "获取页面信息时出错，请重试。";
-            resetProcessingState();
-            return;
-          }
+      const img = document.createElement("img");
+      img.src = url;
+      img.style.maxWidth = "100px";
+      img.style.marginRight = "10px";
 
-          if (injectionResults && injectionResults[0]) {
-            let imageUrls = injectionResults[0].result;
-            // console.log(imageUrls);
-            downloadImagesToLocal(imageUrls);
-            downloadDisplay.textContent = "产品图片下载成功";
-          } else {
-            console.error("未能下载产品图片");
-            downloadDisplay.textContent = "未能下载产品图片，请重试。";
-          }
+      const downloadBtn = document.createElement("button");
+      downloadBtn.textContent = "下载";
+      downloadBtn.onclick = () => downloadImage(url, index);
 
-          resetProcessingState();
-        }
-      );
+      wrapper.appendChild(img);
+      wrapper.appendChild(downloadBtn);
+      container.appendChild(wrapper);
     });
-  });
 
-  // 在内容脚本中执行的函数
+    container.insertAdjacentHTML(
+      "beforebegin",
+      `<p>图片数量：${imageUrls.length}</p>`
+    );
+  }
+
+  function downloadImage(url, index) {
+    chrome.downloads.download(
+      {
+        url: url,
+        filename: `product_image_${index + 1}.jpg`,
+        saveAs: false,
+        conflictAction: "uniquify",
+      },
+      (downloadId) => {
+        if (chrome.runtime.lastError) {
+          console.error(`下载图片时出错:`, chrome.runtime.lastError.message);
+        } else {
+          console.log(`图片下载成功，下载ID: ${downloadId}`);
+        }
+      }
+    );
+  }
+
   function getPageInfo() {
     const productNameElement = document.querySelector(".breadcrumb-btm");
     const productName = productNameElement ? productNameElement.innerText : "";
@@ -132,17 +130,19 @@ document.addEventListener("DOMContentLoaded", function () {
     );
     const origin = originElement ? originElement.innerText : "";
 
-    // TODO: 作用域！！！有待优化！
+    // TODO: 作用域！！！有待优化
     function _getProductImagesUrls() {
       const gallery = document.querySelector(".productImageGallery");
       if (!gallery) return [];
 
       const images = gallery.querySelectorAll("img[data-primaryimagesrc]");
       return Array.from(images).map(
-        (img) => "https:" + img.getAttribute("data-primaryimagesrc")
+        (img) =>
+          "https://" + img.getAttribute("data-primaryimagesrc").split("//")[1]
       );
     }
-    const imgsUrls = _getProductImagesUrls().join("\n");
+    // const imgsUrls = _getProductImagesUrls().join("\n");
+    const imgsUrls = _getProductImagesUrls();
 
     return { productBrand, productName, shortDesc, origin, imgsUrls };
   }
@@ -182,14 +182,13 @@ document.addEventListener("DOMContentLoaded", function () {
     return { categoryPath, productShopId, productSKU };
   }
   // Helper function to save a page title to local file
-  function savePageInfoToFile(content) {
+  function savePageInfoToFile(content, lang) {
     // 使用 \n 来创建换行
     let formattedContent = content.replace(/\n/g, "\r\n");
     // const dateString = getCurrentDateString();
-    const lang = checkStringType(content);
 
     chrome.downloads.download({
-      filename: `${productCode}_${lang}.txt`,
+      filename: `${lang}_${productCode}.txt`,
       saveAs: true,
       conflictAction: "overwrite",
       url:
@@ -200,65 +199,18 @@ document.addEventListener("DOMContentLoaded", function () {
   function getCurrentDateString() {
     const currentDate = new Date();
     const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
     return `${year}${month}${day}`;
   }
 
-  function checkStringType(str) {
-    // 检查是否包含中文字符
-    const hasChineseChar = /[\u4e00-\u9fa5]/.test(str);
-    if (hasChineseChar) {
-      return 'Chinese';
-    }
-  
-    // 检查是否全部由英文字符组成
-    const isEnglish = /^[a-zA-Z]+$/.test(str);
-    if (isEnglish) {
-      return 'English';
-    }
-  
-    // 如果既不是中文也不是英文,返回 'Other'
-    return 'Other';
-  }
-
-  function getProductImagesUrls() {
-    const gallery = document.querySelector(".productImageGallery");
-    if (!gallery) return [];
-
-    const images = gallery.querySelectorAll("img[data-primaryimagesrc]");
-    return Array.from(images).map(
-      (img) => "https://" + img.getAttribute("data-primaryimagesrc").split('//')[1]
-    );
-  }
-  function downloadImagesToLocal(imageUrls) {
-    console.log(imageUrls)
-
-    imageUrls.forEach((url, index) => {
-      chrome.downloads.download(
-        {
-          url: url,
-          filename: `product_image_${index + 1}.jpg`,
-          saveAs: true,
-          conflictAction: "uniquify",
-        },
-        (downloadId) => {
-          if (chrome.runtime.lastError) {
-            console.error(
-              `下载图片 ${url} 时出错:`,
-              chrome.runtime.lastError.message
-            );
-          } else {
-            console.log(`图片 ${url} 下载成功，下载ID: ${downloadId}`);
-          }
-        }
-      );
-    });
+  function isEnglishPage(url) {
+    // 检查URL是否包含'/en/'
+    return url.includes("/en/");
   }
 
   function resetProcessingState() {
     isProcessing = false;
     saveButton.disabled = false; // 重新启用按钮
-    downloadButton.disabled = false; // 重新启用按钮
   }
 });
