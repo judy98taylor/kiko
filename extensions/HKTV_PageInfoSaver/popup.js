753bba01544c2e3fbff08efafacaddcf3664d7e2
taylor <judy1998taylor@qq.com>
2024-08-20 19:37:40 +0800

    feat: Add download button for product information in English and Chinese
    Adds a button that allows users to download the current product information in both English and Chinese.

--- extensions/HKTV_PageInfoSaver/popup.js
+++ extensions/HKTV_PageInfoSaver/popup.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var saveButton = document.getElementById("save-button");
   var saveDisplay = document.getElementById("save-display");
+  let keysDisplay = document.getElementById("keys-display");
+
   var isProcessing = false;
   var productCode;
 
@@ -8,67 +9,139 @@ document.addEventListener("DOMContentLoaded", function () {
     if (chrome.runtime.lastError) {
       console.error("查询标签页时出错：", chrome.runtime.lastError.message);
       saveDisplay.textContent = "获取页面信息时出错，请重试。";
-      resetProcessingState();
       return;
     }
-    const url = tabs[0].url.split('?')[0];
-    const lang = isEnglishPage(url) ? "en" : "zh";
+    const url = tabs[0].url.split("?")[0];
+    if (
+      !url.includes("www.hktvmall.com/hktv/zh/main") &&
+      !url.includes("www.hktvmall.com/hktv/en/main")
+    ) {
+      keysDisplay.textContent = "只适用于产品详情页";
+    } else {
+      showLocalSaveInfo();
 
-    saveButton.addEventListener("click", function () {
-      if (isProcessing) {
-        saveDisplay.textContent = "save操作正在进行中，请稍候...";
-        console.log("save操作正在进行中，请稍候...");
-        return;
-      }
+      const lang = isEnglishPage(url) ? "en" : "cn";
 
-      isProcessing = true;
-      saveButton.disabled = true; // 禁用按钮
+      const saveButton = document.createElement("button");
+      saveButton.textContent = "Save Current Product Info";
+      saveButton.style.color = "blue";
+      saveDisplay.appendChild(saveButton);
+      saveButton.addEventListener("click", function () {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tabs[0].id },
+            func: getPageInfo,
+          },
+          (injectionResults) => {
+            if (chrome.runtime.lastError) {
+              console.error(
+                "执行脚本时出错：",
+                chrome.runtime.lastError.message
+              );
+              saveDisplay.textContent = "获取页面信息时出错，请重试。";
+              return;
+            }
 
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tabs[0].id },
-          func: getPageInfo,
-        },
-        (injectionResults) => {
-          if (chrome.runtime.lastError) {
-            console.error("执行脚本时出错：", chrome.runtime.lastError.message);
-            saveDisplay.textContent = "获取页面信息时出错，请重试。";
-            resetProcessingState();
-            return;
+            if (injectionResults && injectionResults[0]) {
+              let obj_injectionResults = injectionResults[0].result;
+              let obj_extractInfoFromUrl = extractInfoFromUrl(url);
+
+              let infoObject = {
+                categoryPath: obj_extractInfoFromUrl.categoryPath,
+                productBrand: obj_injectionResults.productBrand,
+                productName: obj_injectionResults.productName,
+                shortDesc: obj_injectionResults.shortDesc,
+                origin: obj_injectionResults.origin,
+              };
+
+              // 保存信息到localStorage
+              let infoObject_name =
+                "_" +
+                obj_extractInfoFromUrl.productShopId +
+                "_" +
+                obj_extractInfoFromUrl.productSKU;
+              saveToLocalStorage(infoObject, lang + infoObject_name);
+
+              // 展示当前保存的信息
+              let content = generateContent(infoObject);
+              saveDisplay.textContent = content;
+
+              // 展示图片和下载按钮
+              showImagesWithDownloadButtons(obj_injectionResults.imgsUrls);
+
+              // 保存信息到本地文件
+              // savePageInfoToFile(content, lang);
+
+              showLocalSaveInfo();
+            } else {
+              console.error("未能获取页面信息");
+              saveDisplay.textContent = "未能获取页面信息，请重试。";
+            }
           }
-
-          if (injectionResults && injectionResults[0]) {
-            let obj_injectionResults = injectionResults[0].result;
-            let obj_extractInfoFromUrl = extractInfoFromUrl(url);
-
-            let infoObject = {
-              productName: obj_injectionResults.productName,
-              productBrand: obj_injectionResults.productBrand,
-              shortDesc: obj_injectionResults.shortDesc,
-              origin: obj_injectionResults.origin,
-              categoryPath: obj_extractInfoFromUrl.categoryPath,
-            };
-
-            let content = generateContent(infoObject);
-            saveDisplay.textContent = content;
-
-            // 显示图片和下载按钮
-            displayImagesWithDownloadButtons(obj_injectionResults.imgsUrls);
-
-            // 保存信息到本地文件
-            savePageInfoToFile(content, lang);
-          } else {
-            console.error("未能获取页面信息");
-            saveDisplay.textContent = "未能获取页面信息，请重试。";
-          }
-
-          resetProcessingState();
-        }
-      );
-    });
+        );
+      });
+    }
   });
 
-  function displayImagesWithDownloadButtons(imageUrls) {
+  function showLocalSaveInfo() {
+    keysDisplay.textContent = "";
+
+    let keys = Object.keys(localStorage);
+    if (keys.length > 0) {
+      const clearButton = document.createElement("button");
+      clearButton.textContent = "Delete the saved data";
+      clearButton.style.color = "red";
+      keysDisplay.appendChild(clearButton);
+      clearButton.onclick = () => {
+        localStorage.clear();
+        keysDisplay.textContent = "已清除所有";
+      };
+
+      const keyList = document.createElement("p");
+      keyList.textContent = keys.join(" ; ");
+      clearButton.after(keyList);
+
+      function findDifferentPrefixKeys(keys) {
+        const cnKeys = keys.filter((key) => key.startsWith("cn_"));
+        const enKeys = keys.filter((key) => key.startsWith("en_"));
+        const commonSuffixes = cnKeys
+          .map((key) => key.substring(3))
+          .filter((suffix) => enKeys.some((enKey) => enKey.endsWith(suffix)));
+        return commonSuffixes;
+      }
+      const commonKeys = findDifferentPrefixKeys(keys);
+      console.log("找到开头不同后面相同的元素:", commonKeys);
+
+      commonKeys.forEach((key) => {
+        const button = document.createElement("button");
+        button.textContent = "Download " + key;
+        button.style.color = "green";
+        keyList.after(button);
+
+        button.onclick = () => {
+          const cnKey = localStorage.getItem(`cn_${key}`);
+          const enKey = localStorage.getItem(`en_${key}`);
+          if (cnKey && enKey) {
+            const cnData = JSON.parse(cnKey);
+            const enData = JSON.parse(enKey);
+            const mergedData = {};
+            Object.keys(enData).forEach((key) => {
+              mergedData[`${key}_1_en`] = enData[key];
+            });
+            Object.keys(cnData).forEach((key) => {
+              mergedData[`${key}_2_cn`] = cnData[key];
+            });
+            console.log("合并后的数据:", mergedData);
+            const sortedMergedData = sortObjectKeys(mergedData);
+            console.log("sort后的数据:", sortedMergedData);
+            savePageInfoToFile(generateContent(sortedMergedData), "fullLang");
+          }
+        };
+      });
+    }
+  }
+
+  function showImagesWithDownloadButtons(imageUrls) {
     const container = document.getElementById("image-container");
     container.innerHTML = ""; // 清空容器
 
@@ -178,12 +251,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     return { categoryPath, productShopId, productSKU };
   }
-  // Helper function to save a page title to local file
+
   function savePageInfoToFile(content, lang) {
     // 使用 \n 来创建换行
     let formattedContent = content.replace(/\n/g, "\r\n");
     // const dateString = getCurrentDateString();
-    alert(`${lang}_${productCode}.txt`)
     chrome.downloads.download({
       filename: `${lang}_${productCode}.txt`,
       saveAs: true,
@@ -191,6 +263,10 @@ document.addEventListener("DOMContentLoaded", function () {
       url:
         "data:text/plain;charset=utf-8," + encodeURIComponent(formattedContent),
     });
+  }
+
+  function saveToLocalStorage(obj, key) {
+    localStorage.setItem(key, JSON.stringify(obj));
   }
 
   function getCurrentDateString() {
@@ -206,8 +282,16 @@ document.addEventListener("DOMContentLoaded", function () {
     return url.includes("/en/");
   }
 
-  function resetProcessingState() {
-    isProcessing = false;
-    saveButton.disabled = false; // 重新启用按钮
+  function sortObjectKeys(obj) {
+    // 获取对象的键名
+    const keys = Object.keys(obj).sort();
+
+    // 使用 reduce() 方法将排序后的键名和对应值重新组装成对象
+    const sortedObj = keys.reduce((acc, key) => {
+      acc[key] = obj[key];
+      return acc;
+    }, {});
+
+    return sortedObj;
   }
 });
